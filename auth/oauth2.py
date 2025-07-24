25f1887ade12ab5a39368b7250199528fe9e3043
ambiwalencja <helena.a.sokolowska@gmail.com>
2025-07-24 22:28:08 +0200

    added refresh token logic, added functions get_user_from_token and get_user_from_token_raw, more flexible version of get_current_user, and simplified setting expiration time for tokens

--- auth/oauth2.py
+++ auth/oauth2.py
@@ -1,5 +1,5 @@
 from fastapi.security import OAuth2PasswordBearer
-from typing import Optional
+# from typing import Optional
 from datetime import datetime, timedelta, timezone
 from jose import jwt, JWTError # pip install python-jose[cryptography]
 from fastapi import HTTPException, Depends, status
@@ -8,22 +8,29 @@ from db.db_connect import get_db
 from utils import user_functions
 # from schemas.users_schemas import UserSignIn
 import os
+from db_models.user_data import User
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="user/login_form", )
 
 SECRET_KEY = os.environ.get('SECRET_KEY')
 ALGORITHM = 'HS256'
 ACCESS_TOKEN_EXPIRE_MINUTES = int(os.environ.get('ACCESS_TOKEN_EXPIRE_MINUTES'))
+REFRESH_TOKEN_EXPIRE_DAYS = int(os.environ.get('REFRESH_TOKEN_EXPIRE_DAYS'))
 
 # tworzenie tokena
-def create_access_token(data: dict, expires_delta: Optional[timedelta] = None):
-  to_encode = data.copy()
-  if expires_delta:
-    expire = datetime.now(timezone.utc) + expires_delta #tutaj jest timezone.utc - w innych miejscach mamy po prostu datetime.now
-    # możliwe, że ktoś, np mama, będzie się logować z kraju z inną strefą, więc dobrze byłoby to obsłużyć. ale chyba na ten moment nie trzeba
-  else:
+def create_access_token(data: dict):
+    to_encode = data.copy()
     expire = datetime.now(timezone.utc) + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
-  to_encode.update({"exp": expire})
+    # tutaj jest timezone.utc - w innych miejscach mamy po prostu datetime.now
+    # możliwe, że ktoś, np mama, będzie się logować z kraju z inną strefą, więc dobrze byłoby to obsłużyć. ale chyba na ten moment nie trzeba
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
+
+def create_refresh_token(data: dict):
+  to_encode = data.copy()
+  expire = datetime.now(timezone.utc) + timedelta(days=REFRESH_TOKEN_EXPIRE_DAYS)
+  to_encode.update({"exp": expire, "scope": "refresh_token"})
   encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
   return encoded_jwt
 
@@ -46,5 +53,29 @@ def get_current_user(token: str = Depends(oauth2_scheme), db: Session = Depends(
     raise credentials_exception
   return user
 
+# weryfikacja tokenu/tożsamości - ver 2 - obsługująca access i refresh token
+def get_user_from_token_raw(token: str, expected_scope: str, db: Session) -> User:
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        if payload.get("scope") != expected_scope:
+            raise JWTError("Invalid token scope")
+    except JWTError:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Invalid or expired token"
+        )
+    
+    username = payload.get("username")
+    if not username:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Missing username in token")
+    
+    user = user_functions.get_user_by_username(db, username=username)
+    if user is None:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="User not found")
+    
+    return user
 
-
+def get_user_from_token(expected_scope: str):
+    def dependency(token: str = Depends(oauth2_scheme), db: Session = Depends(get_db)):
+        return get_user_from_token_raw(token, expected_scope, db)
+    return dependency
--- routers/user_endpoints.py
+++ routers/user_endpoints.py
@@ -1,5 +1,5 @@
 from sqlalchemy.orm.session import Session
-from fastapi import APIRouter, Depends, HTTPException, status
+from fastapi import APIRouter, Depends, HTTPException, status, Response, Request
 from fastapi.security import OAuth2PasswordRequestForm
 import os
 from db.db_connect import get_db
@@ -7,8 +7,11 @@ from schemas.user_schemas import UserBase, UserDisplay, UserSignIn
 from db_models.user_data import User
 from utils import user_functions
 from auth.hashing import Hash
-from auth.oauth2 import create_access_token, get_current_user
+from auth.oauth2 import create_access_token, create_refresh_token, get_current_user
+from auth.oauth2 import get_user_from_token_raw, get_user_from_token
 
+REFRESH_TOKEN_EXPIRE_DAYS = int(os.environ.get('REFRESH_TOKEN_EXPIRE_DAYS'))
+REFRESH_TOKEN_EXPIRE_SECONDS = REFRESH_TOKEN_EXPIRE_DAYS * 24 * 60 * 60
 
 router = APIRouter(
     prefix="/user",
@@ -31,41 +34,119 @@ def create_user(request: UserBase, passphrase: str, db: Session = Depends(get_db
 
 
 @router.post('/login')
-def login(request: UserSignIn, db: Session = Depends(get_db)):
+# TODO: tutaj response dodany w body
+def login(request: UserSignIn, response: Response, db: Session = Depends(get_db)):
     user = user_functions.get_user_by_username(db, request.username)
     if not Hash.verify(user.Password, request.password):
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,
             detail='Incorrect password')
     
     access_token = create_access_token(data={'username': user.Username})
+    refresh_token = create_refresh_token(data={"username": user.Username})
     
     user_functions.update_last_login(db, user)
 
+    #TODO: to dodane - zapisywanie refresh tokenu do cookie
+    response.set_cookie(
+        key="refresh_token",
+        value=refresh_token,
+        httponly=True,
+        secure=True,
+        samesite="strict",
+        max_age=REFRESH_TOKEN_EXPIRE_SECONDS
+    )
+
     return {
         'access_token': access_token,
+        "refresh_token": refresh_token,
         'token_type': 'bearer',
         'user_id': user.ID_uzytkownika,
         'username': user.Username,
         'role': user.Role
     }
 
-@router.post('/login_form')
-def login(form_data: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db)): # mogą sie tak samo nazywać dwa endpointy?
+@router.post('/login_form') # do testowania, do autoryzacji w docsach
+def login_form(response: Response, form_data: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db)):
     user = user_functions.get_user_by_username(db, form_data.username)
     if not Hash.verify(user.Password, form_data.password):
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,
             detail='Incorrect password')
     
     access_token = create_access_token(data={'username': user.Username})
+    refresh_token = create_refresh_token(data={"username": user.Username})
+    
+    response.set_cookie(
+        key="refresh_token",
+        value=refresh_token,
+        httponly=True,
+        secure=True,
+        samesite="strict",
+        max_age=REFRESH_TOKEN_EXPIRE_SECONDS
+    )
 
     return {
         'access_token': access_token,
+        "refresh_token": refresh_token,
         'token_type': 'bearer'
     }
 
+# TODO: wersja refresha bez zapisywania tokenu w cookiesie
+# @router.post('/refresh')
+# def refresh_token(refresh_token: str, db: Session = Depends(get_db)):
+#     try:
+#         payload = jwt.decode(refresh_token, SECRET_KEY, algorithms=[ALGORITHM])
+#         if payload.get("scope") != "refresh_token":
+#             raise HTTPException(status_code=401, detail="Invalid scope")
+#     except JWTError:
+#         raise HTTPException(status_code=401, detail="Invalid refresh token")
 
-@router.get('/me', response_model=UserDisplay) # 
+#     user = user_functions.get_user_by_username(db, payload.get("username"))
+
+#     new_access_token = create_access_token(data={"username": user.Username})
+#     return {
+#         "access_token": new_access_token,
+#         "token_type": "bearer"
+#     }
+
+# TODO: wersja z cookie - z dekodowaniem tokenu na miejscu - potrzebny wtedy import jwt
+# @router.post("/refresh")
+# def refresh_token(request: Request):
+#     refresh_token = request.cookies.get("refresh_token")
+#     if not refresh_token:
+#         raise HTTPException(status_code=401, detail="No refresh token")
+
+#     try:
+#         payload = jwt.decode(refresh_token, SECRET_KEY, algorithms=[ALGORITHM])
+#         if payload.get("scope") != "refresh_token":
+#             raise HTTPException(status_code=401, detail="Invalid scope")
+#     except JWTError:
+#         raise HTTPException(status_code=401, detail="Invalid refresh token")
+
+#     new_access_token = create_access_token({"username": payload.get("username")})
+#     return {"access_token": new_access_token}
+
+# wersja z cookie - z dekodowaniem tokenu w osobnej funkcji w oauth2
+@router.post("/refresh2")
+def refresh_token2(request: Request, db: Session = Depends(get_db)):
+    refresh_token = request.cookies.get("refresh_token")
+    if not refresh_token:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="No refresh token")
+
+    user = get_user_from_token_raw(refresh_token, "refresh_token", db)
+
+    new_access_token = create_access_token({"username": user.Username})
+    return {"access_token": new_access_token}
+
+
+# ver 1 - z użyciem get_current_user
+@router.get('/me', response_model=UserDisplay)
 def get_current_user_info(current_user: User = Depends(get_current_user)):
+    return current_user
+
+
+# ver 2 - z użyciem get_user_from_token (dla access i refresh tokena)
+@router.get("/me", response_model=UserDisplay)
+def get_me(current_user: User = Depends(get_user_from_token("access_token"))):
     return current_user
 
 
@@ -83,3 +164,9 @@ def reset_password_for_user(request: UserSignIn, db: Session = Depends(get_db), 
     db.commit()
     db.refresh(user)
     return True
+
+#TODO: czat zaproponował też taki endpoint:
+@router.post('/logout')
+def logout(response: Response):
+    response.delete_cookie("refresh_token")
+    return {"message": "Logged out"}
