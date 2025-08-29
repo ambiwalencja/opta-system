dd3d842c562760733070a6f6bea75a96816088ac
noCR4SH <wojciech.niekrasz@gmail.com>
2025-08-29 11:09:08 +0200

    merge from develop (#3)
    * added refresh token logic, added functions get_user_from_token and get_user_from_token_raw, more flexible version of get_current_user, and simplified setting expiration time for tokens
    
    * changed refresh endpoint - refresh token is being retrieved from request body now
    
    * added display_users and delete_user endpoints
    
    * added Created date to display users and formatted dates displayed
    
    * fixed: display users (get, check user role), delete user (by id), refresh token (schema instead of Request)
    
    * started adding schemas and endpoints for clients - added first version of ClientCreate schema, create endpoint and create function
    
    * added endpoint for populating possible_values table, then schema and endpoint for creating pacjent, including validation against possible_values and checking for duplicates
    
    * changed user deletion by ID to by username; started connecting to old database
    
    * added first functions and endpoints for importing and transforming tables from old database - pacjenci table
    
    * added schema PacjentDisplay, added columns for 'other' response text to Pacjent model and CreatePacjent schema, and added them to transform_table_pacjenci
    
    ---------
    
    Co-authored-by: ambiwalencja <helena.a.sokolowska@gmail.com>

--- schemas/user_schemas.py
+++ schemas/user_schemas.py
@@ -1,6 +1,5 @@
 from pydantic import BaseModel
 from pydantic.fields import Field
-# from typing import List
 from enum import Enum
 
 class RoleEnum(str, Enum):
@@ -16,10 +15,14 @@ class UserBase(BaseModel):
 class UserDisplay(BaseModel):
     username: str = Field(alias='Username', serialization_alias='username')
     role: str = Field(alias='Role', serialization_alias='role')
-    class Config(): # to robi, że klasa UserDisplay rozumie, czyta obiekty sqlalchemy i jest w stanie zmapować na jsona
-        from_attributes = True
+    class Config(): 
+        from_attributes = True # to robi, że klasa UserDisplay rozumie, czyta obiekty sqlalchemy i jest w stanie zmapować na jsona
+        # bez tego trzeba byłoby podawać dane w postaci dicta, jsona, a tak można 
 
 
 class UserSignIn(BaseModel):
     username: str
     password: str
+
+class TokenRequest(BaseModel):
+    refresh_token: str
---
+++ utils/client_functions.py
@@ -0,0 +1,45 @@
+
+from db_models.client_data import Pacjent
+from db_models.config import PossibleValues
+from schemas.client_schemas import CreatePacjent
+from sqlalchemy.orm.session import Session
+from auth.hashing import Hash
+from datetime import datetime
+from fastapi import HTTPException, status
+
+def create_pacjent(db: Session, pacjent_data: CreatePacjent):
+    # Convert to dict with DB column names
+    data_dict = pacjent_data.model_dump(by_alias = True, exclude_unset = True)
+
+    # Add backend-generated fields
+    data_dict["Created"] = datetime.now()
+    data_dict["Last_modified"] = datetime.now()
+
+    # Conditional cleanup - optional
+    if not data_dict.get("Niebieska_karta"):
+        data_dict["Niebieska_karta_inicjator"] = None
+        data_dict["Grupa_robocza"] = None
+        data_dict["Grupa_robocza_sklad"] = None
+        data_dict["Plan_pomocy"] = None
+        data_dict["Plan_pomocy_opis"] = None
+        data_dict["Narzedzia_prawne"] = None
+        data_dict["Zawiadomienie"] = None
+    
+    # Create SQLAlchemy object
+    new_pacjent = Pacjent(**data_dict)
+    
+    db.add(new_pacjent)
+    db.commit()
+    db.refresh(new_pacjent)
+    return new_pacjent
+
+def validate_choice(db: Session, variable_name: str, chosen_value: str):
+    pv = db.query(PossibleValues).filter(PossibleValues.Variable_name == variable_name).first()
+    if not pv:
+        return  # no restriction for this field → skip validation
+    
+    if chosen_value not in pv.Possible_values:  # dict keys are the valid values
+        raise HTTPException(
+            status_code=400,
+            detail=f"Invalid value '{chosen_value}' for {variable_name}. Allowed: {list(pv.Possible_values.keys())}"
+        )
