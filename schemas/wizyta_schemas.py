9688047aecc6c54f937a7ad255019bd4d5048c8a
ambiwalencja <helena.a.sokolowska@gmail.com>
2026-02-21 01:05:05 +0100

    updated endpoint detailed_wizyty_for_pacjent - added user full name to response

--- schemas/wizyta_schemas.py
+++ schemas/wizyta_schemas.py
@@ -27,10 +27,11 @@ class WizytaIndywidualnaImport(WizytaIndywidualnaCreate):
 class WizytaIndywidualnaDisplay(BaseModel):
     id_wizyty: int = Field(..., alias="ID_wizyty")
     id_pacjenta: int = Field(..., alias="ID_pacjenta")
+    id_uzytkownika: int = Field(..., alias="ID_uzytkownika")
     data_wizyty: date = Field(..., alias="Data_wizyty")
     typ_wizyty: str = Field(..., alias="Typ_wizyty")
     liczba_godzin: float = Field(..., alias="Liczba_godzin")
-    id_uzytkownika: int = Field(..., alias="ID_uzytkownika")
+    
 
 class WizytaIndywidualnaUpdate(WizytaIndywidualnaCreate):
     id_pacjenta: Optional[int] = Field(None, alias="ID_pacjenta")
@@ -38,4 +39,31 @@ class WizytaIndywidualnaUpdate(WizytaIndywidualnaCreate):
     data_wizyty: Optional[date] = Field(None, alias="Data_wizyty")
     typ_wizyty: Optional[str] = Field(None, alias="Typ_wizyty")
     liczba_godzin: Optional[float] = Field(None, alias="Liczba_godzin")
+
+class UserDisplayShort(BaseModel):
+    id_uzytkownika: int = Field(..., alias="ID_uzytkownika")
+    full_name: str = Field(..., alias="Full_name")
+
+    class Config():
+        from_attributes = True
+        validate_by_name = True
+        validate_by_alias = True
+
+class WizytaIndywidualnaDisplayDetails(BaseModel):
+    id_wizyty: int = Field(..., alias="ID_wizyty")
+    id_pacjenta: int = Field(..., alias="ID_pacjenta")
+    data_wizyty: date = Field(..., alias="Data_wizyty")
+    typ_wizyty: str = Field(..., alias="Typ_wizyty")
+    liczba_godzin: float = Field(..., alias="Liczba_godzin")
+    notatka_diagnoza_sytuacji: Optional[str] = Field(None, alias="Notatka_diagnoza_sytuacji")
+    notatka_opis_sytuacji: Optional[str] = Field(None, alias="Notatka_opis_sytuacji")
+    notatka_indywidualny_plan: Optional[str] = Field(None, alias="Notatka_indywidualny_plan")
+    notatka_rezultaty: Optional[str] = Field(None, alias="Notatka_rezultaty")
+    notatka_odeslanie_do_innych: Optional[str] = Field(None, alias="Notatka_odeslanie_do_innych")
+    
+    # User
+    uzytkownik: UserDisplayShort = Field(...)
+    
+
+
     
